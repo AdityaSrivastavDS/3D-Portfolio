83dc9ba7d319574e7f5e542ead38510d19514f87
ADITYA SRIVASTAV <114422969+AdityaSrivastavDS@users.noreply.github.com>
2024-07-19 02:16:28 -0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import { peroptyx, digitmg , mahir, headstarter} from "../assets/images";
+import { peroptyx, digitmg , mahir, headstarter} from ".src/assets/images";
 import {
     car,
     contact,
