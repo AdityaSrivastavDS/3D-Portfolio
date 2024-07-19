802f4c727990c896702648b29e929dd97a54cc1f
ADITYA SRIVASTAV <adityathestar2006@gmail.com>
2024-07-19 02:04:00 -0700

    Port

---
+++ src/constants/index.js
@@ -0,0 +1,321 @@
+import { peroptyx, digitmg , mahir, headstarter} from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: "https://img.icons8.com/?size=100&id=hGdCwhSHUe6L&format=png&color=000000",
+        name: "Python",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "SWE Fellow",
+        company_name: "Headstarter AI",
+        icon: headstarter,
+        iconBg: "#accbe1",
+        date: "July 2024 - Ongoing",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Student Ambassador",
+        company_name: "Microsoft",
+        icon: "https://img.icons8.com/?size=100&id=22989&format=png&color=000000",
+        iconBg: "#fbc3bc",
+        date: "October 2023 - Ongoing",
+        points: [
+            "Orchestrated 5 hackathons, conducted 10 coding competitions, and hosted 15 tech talks, yielding a 30% enhancement in students' technical proficiency.",
+            "Guided 3 collaborative projects, demonstrating practical applications of Microsoft technologies, earning recognition and adoption from industry experts.",
+            "Mentored 100 students, leading to 20 successful placements in top technology firms.",
+        ],
+    },
+    {
+        title: "Founder",
+        company_name: "Mahir Insight Institute",
+        icon: mahir,
+        iconBg: "#b7e4c7",
+        date: "September 2023 - Forever",
+        points: [
+            "Developing and maintaining educational programs and resources using cutting-edge data science and AI technologies.",
+            "Collaborating with cross-functional teams including instructors, curriculum designers, and industry experts to create high-quality educational content.",
+            "Implementing innovative teaching methodologies and ensuring the accessibility and effectiveness of learning materials across different platforms.",
+            "Participating in strategic planning sessions and providing constructive feedback to team members to drive continuous improvement and organizational growth.",
+        ],
+    },
+    {
+        title: "Tech Venture Capitalist Intern",
+        company_name: "Hp",
+        icon: "https://img.icons8.com/?size=100&id=69455&format=png&color=000000",
+        iconBg: "#a2d2ff",
+        date: "May 2024 - July 2024",
+        points: [
+            "Conducted market analysis for emerging technologies, resulting in the identification of three high-potential tech ventures, contributing to a 15% increase in potential investment.",
+            "Developed financial models and projections for start-ups, improving the accuracy of financial forecasts by 20%.",
+            "Presented findings and strategic recommendations to senior management, enhancing decision making process and securing approval for two new investments.",
+            " Collaborated with cross functional teams to access the viability of new tech ventures, leading to the successful vetting of five start-ups for further consideration.",
+        ],
+    },
+    {
+        title: "Data Analyst | FTE",
+        company_name: "Peroptyx",
+        icon: peroptyx,
+        iconBg: "#a2d2ff",
+        date: "June 2024 - July 2024",
+        points: [
+            "Developing and maintaining data models, dashboards, and reports using SQL, Power BI, and other related technologies.",
+            "Collaborating with cross-functional teams including data engineers, product managers, and other stakeholders to deliver high-quality data solutions.",
+            "Implementing data visualization techniques and ensuring the accuracy and consistency of data across different sources.",
+            "Participating in data reviews and providing constructive feedback on data quality, analysis methodologies, and insights.",
+        ],
+    },
+    {
+        title: "Data Science Trainee",
+        company_name: "360DigiTMG",
+        icon: digitmg,
+        iconBg: "#a2d2ff",
+        date: "October 2023 - November 2023",
+        points: [
+            "Developing and maintaining machine learning models, data pipelines, and analytical reports using Python, R, and other related technologies.",
+            "Collaborating with cross-functional teams including data scientists, data engineers, and business analysts to deliver impactful data-driven solutions.",
+            "Implementing data preprocessing and feature engineering techniques to ensure the quality and consistency of data used in models.",
+            "Participating in code and model reviews, providing constructive feedback on data analysis, modeling approaches, and derived insights.",
+        ],
+    },
+    {
+        title: "Business Analyst Intern",
+        company_name: "Tata",
+        icon: "https://img.icons8.com/?size=100&id=pTx0jdlttlwQ&format=png&color=000000",
+        iconBg: "#a2d2ff",
+        date: "August 2023 - August 2023",
+        points: [
+            "Engineered precise and expert-level interactive data visualizations and dashboards, significantly enhancing data comprehension and improving decision-making efficiency by 20%.",
+            "Delivered impactful data visualizations, resulting a 20% increase in the efficiency of data-driven decision-making processes.",
+        ],
+    },
+    {
+        title: "Data Analyst Intern",
+        company_name: "IBM",
+        icon: "https://img.icons8.com/?size=100&id=31754&format=png&color=000000",
+        iconBg: "#a2d2ff",
+        date: "June 2023 - July 2023",
+        points: [
+            "Engineered data visualizations that significantly enhanced data understanding, accelerating decision-making by 8%",
+            "Conducted rigorous data cleaning and preprocessing, achieving a notable 4% improvement in data quality and accuracy.",
+            "Analyzed large datasets to identify trends and patterns, providing actionable insights for business decision-making.",
+            "Collaborated with cross-functional teams to identify critical business challenges and opportunities for effective application of data science techniques, resulting in implementing data-driven solutions that increased cost savings by 25%",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/AdityaSrivastavDS',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/adityamlsa/',
+    },
+    {
+        name: 'Devfolio',
+        iconUrl: "https://img.icons8.com/?size=100&id=Sf2NuZRCVuaE&format=png&color=000000",
+        link: 'https://devfolio.co/@AdityaDS',
+    },
+    {
+        name: 'Comuddle',
+        iconUrl: "https://img.icons8.com/?size=100&id=eMdBqh1N9IWw&format=png&color=000000",
+        link: 'https://www.commudle.com/users/AdityaDS',
+    },
+    {
+        name: 'Instagram',
+        iconUrl: "https://img.icons8.com/?size=100&id=nj0Uj45LGUYh&format=png&color=000000",
+        link: 'https://www.instagram.com/aditya_srivastav_ds/',
+    },
+    {
+        name: 'Twitter',
+        iconUrl: "https://img.icons8.com/?size=100&id=111057&format=png&color=000000",
+        link: 'https://twitter.com/Aditya_DSAI',
+    }
+    ,
+    {
+        name: 'Codechef',
+        iconUrl: "https://img.icons8.com/?size=100&id=vAtJFm3hwtQw&format=png&color=000000",
+        link: 'https://www.codechef.com/users/aditya_ds',
+    }
+    ,
+    {
+        name: 'HackerRank',
+        iconUrl: "https://img.icons8.com/?size=100&id=OUPsEPLKIebZ&format=png&color=000000",
+        link: 'https://www.hackerrank.com/profile/aditya_ds',
+    }
+    ,
+    {
+        name: 'Leetcode',
+        iconUrl: "https://img.icons8.com/?size=100&id=wDGo581Ea5Nf&format=png&color=000000",
+        link: 'https://leetcode.com/u/Aditya_DSAI/',
+    }
+    ,
+    {
+        name: 'GeeksForGeeks',
+        iconUrl: "https://img.icons8.com/?size=100&id=AbQBhN9v62Ob&format=png&color=000000",
+        link: 'https://www.geeksforgeeks.org/user/adityathex00q/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Amazon Price Tracker',
+        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
+        link: 'https://github.com/adrianhajdin/pricewise',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Full Stack Threads Clone',
+        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+        link: 'https://github.com/adrianhajdin/threads',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Car Finding App',
+        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
+        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Full Stack Instagram Clone',
+        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+        link: 'https://github.com/adrianhajdin/social_media_app',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Real-Estate Application',
+        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+        link: 'https://github.com/adrianhajdin/projects_realestate',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'AI Summarizer Application',
+        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    }
+];
