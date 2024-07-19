c9d68cf9e32b03cc85ae0de5cd79b4c5f79a830c
ADITYA SRIVASTAV <adityathestar2006@gmail.com>
2024-07-19 06:15:20 -0700

    commit update

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -10,7 +10,7 @@ const HomeInfo = ({ currentStage }) => {
         <span className='font-semibold mx-2 text-white'>Aditya</span>
         👋
         <br />
-        A Software Engineer from India
+        An aspiring Data Scientist from India
       </h1>
     );
 
@@ -33,7 +33,7 @@ const HomeInfo = ({ currentStage }) => {
     return (
       <div className='info-box'>
         <p className='font-medium text-center sm:text-xl'>
-          Led multiple projects to success over the years. <br /> Curious about the impact?
+          Led multiple projects to success. <br /> Curious about the impact?
         </p>
 
         <Link to='/projects' className='neo-brutalism-white neo-btn'>
